f99a9e9bd60ef341d69ace170fc66e95de6dd0a4
Plexonic-InstigateMobile <plexonic@instigatemobile.com>
2014-02-21 15:09:08 +0400

    Added initial interface for PSSaltr class.

---
+++ Saltr/Constants.h
@@ -0,0 +1,21 @@
+//
+//  Constants.h
+//  Saltr
+//
+//  Created by Instigate Mobile on 2/21/14.
+//  Copyright (c) 2014 Plexonic. All rights reserved.
+//
+
+#ifndef Saltr_Constants_h
+#define Saltr_Constants_h
+
+#define SALTR_API_URL "http://api.saltr.com/httpjson.action";
+#define SALTR_URL "http://saltr.com/httpjson.action";
+#define COMMAND_APP_DATA "APPDATA";
+#define COMMAND_ADD_PROPERTY "ADDPROP";
+#define COMMAND_SAVE_OR_UPDATE_FEATURE "SOUFTR";
+
+#define PROPERTY_OPERATIONS_INCREMENT "inc";
+#define PROPERTY_OPERATIONS_SET "set";
+
+#endif
