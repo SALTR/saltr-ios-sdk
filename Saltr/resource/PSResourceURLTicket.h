d82d92a2b363999bc6af48a0b4a9d0360c0ee67f
Plexonic-InstigateMobile <plexonic@instigatemobile.com>
2014-03-05 00:06:56 +0400

    Added implementation for PSResourceURLTicket class.

--- Saltr/resource/PSResourceURLTicket.h
+++ Saltr/resource/PSResourceURLTicket.h
@@ -10,6 +10,70 @@
 
 #import <Foundation/Foundation.h>
 
-@interface PSResourceURLTicket : NSObject
+/// The class is for creating the URL request. The class is created to correspond
+// the URLRequest class in action script. @todo uncommented properties do not
+// have matches in NSURLRequest class of ios.
+@interface PSResourceURLTicket : NSObject {
+    
+}
+
+@property (nonatomic, assign) BOOL authenticate;
+@property (nonatomic, assign) BOOL cacheResponse;
+@property (nonatomic, strong) NSString* contentType;
+
+/// The body of HTTP request
+@property (nonatomic, strong) NSData* variables;
+
+@property (nonatomic, assign) BOOL followRedirects;
+
+
+/**
+ * @brief The timeout interval, in seconds. If during a connection attempt the request
+ * remains idle for longer than the timeout interval, the request is considered to have timed out. The
+ * default timeout interval is 60 seconds.
+ */
+@property (nonatomic, assign) NSInteger idleTimeout;
+/// YES if the receiver should use the default cookie handling for the request, NO otherwise. The default is YES
+@property (nonatomic, assign) BOOL manageCookies;
+
+/// HTTP request method
+@property (nonatomic, strong) NSString* method;
+
+/// HTTP headers
+@property (nonatomic, strong) NSDictionary* requestHeaders;
+
+/// the new URL
+@property (nonatomic, strong) NSString* url;
+@property (nonatomic, assign) BOOL useCache;
+@property (nonatomic, strong) NSString* userAgent;
+@property (nonatomic, assign) NSInteger maxAttemps;
+@property (nonatomic, assign) BOOL checkPolicy;
+@property (nonatomic, assign) BOOL useSameDomain;
+@property (nonatomic, assign) NSInteger dropTimeout;
+
+/**
+ * @brief Initializes the current request with the given parameters
+ * @param urlString - the url string of the current request
+ * @param variables - the new request body
+ * @return - initialized object of the @b PSResourceURLTicket class;
+ */
+-(id) initWithURL:(NSString *)urlString andVariables:(NSData *)variables;
+
+/// Returns current url request
+ -(NSURLRequest *) urlRequest;
+
+/**
+ * @brief adds HTTP header to the current request
+ * @param headerName - the name of the header to be added the request
+ * @param headerValue - the value of the header
+ */
+-(void) addHeader:(NSString *)headerName andHeaderValue:(NSString *)headerValue;
+
+/**
+ * @brief Gets the header with the given name
+ * @param headerName - the name of header to be obtained
+ * @return - the header value
+ */
+-(NSString *)headerValue:(NSString *)headerName;
 
 @end
--- Saltr/resource/PSResourceURLTicket.m
+++ Saltr/resource/PSResourceURLTicket.m
@@ -12,4 +12,65 @@
 
 @implementation PSResourceURLTicket
 
+@synthesize authenticate= _authenticate;
+@synthesize cacheResponse=_cacheResponse;
+@synthesize contentType=_contentType;
+@synthesize variables=_variables;
+@synthesize followRedirects=_followRedirects;
+@synthesize idleTimeout=_idleTimeout;
+@synthesize manageCookies=_manageCookies;
+@synthesize method=_method;
+@synthesize requestHeaders=_requestHeaders;
+@synthesize url=_url;
+@synthesize useCache=_useCache;
+@synthesize userAgent=_userAgent;
+@synthesize maxAttemps=_maxAttemps;
+@synthesize checkPolicy=_checkPolicy;
+@synthesize useSameDomain=_useSameDomain;
+@synthesize dropTimeout=_dropTimeout;
+
+-(id) initWithURL:(NSString *)urlString andVariables:(NSData *)variables {
+    self = [super init];
+    if (self) {
+        _url = urlString;
+        _variables = variables;
+        _authenticate = YES;
+        _cacheResponse = YES;
+        _followRedirects = YES;
+        _manageCookies = YES;
+        _useCache = YES;
+        _idleTimeout = 0;
+        _method = @"GET";
+        _checkPolicy = NO;
+        _maxAttemps = 3;
+        _useSameDomain = YES;
+        _dropTimeout = 0;
+
+    }
+    return self;
+}
+
+-(NSURLRequest *) urlRequest {
+    NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:_url]];
+    request.timeoutInterval = _idleTimeout;
+    [request setHTTPShouldHandleCookies:_manageCookies];
+    [request setHTTPMethod:_method];
+    [request setHTTPBody:_variables];
+    [request setAllHTTPHeaderFields:_requestHeaders];
+    return request;
+}
+
+-(void) addHeader:(NSString *)headerName andHeaderValue:(NSString *)headerValue {
+    [_requestHeaders setValue:headerName forKey:headerValue];
+}
+
+-(NSString *)headerValue:(NSString *)headerName {
+    for (NSDictionary* requestHeader in _requestHeaders) {
+        if ([[requestHeader allKeys] containsObject:headerName]) {
+            return [requestHeader valueForKey:headerName];
+        }
+    }
+    return nil;
+}
+
 @end
