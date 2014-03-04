5dece82779f5c44df742df6e3c9ca419e7b47d5a
Plexonic-InstigateMobile <plexonic@instigatemobile.com>
2014-03-05 00:06:04 +0400

    Added ResourceURLTicketTests empty test class.

---
+++ SaltrTests/ResourceURLTicketTests.m
@@ -0,0 +1,34 @@
+//
+//  ResourceURLTicketTests.m
+//  Saltr
+//
+//  Created by Gohar Hovhannisyan on 3/4/14.
+//  Copyright (c) 2014 Plexonic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ResourceURLTicketTests : XCTestCase
+
+@end
+
+@implementation ResourceURLTicketTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
