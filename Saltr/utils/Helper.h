bd8edd1d4090cedb81900f829a6eab97a283f991
Plexonic-InstigateMobile <plexonic@instigatemobile.com>
2014-03-10 17:20:02 +0400

    Added inital implementation for PSResource class.
    1. Added tests for PSSaltr class (30%).
    2. Reimplemented some functionality in PSSaltr class.

---
+++ Saltr/utils/Helper.h
@@ -0,0 +1,13 @@
+//
+//  Helper.h
+//  Saltr
+//
+//  Created by Gohar Hovhannisyan on 3/10/14.
+//  Copyright (c) 2014 Plexonic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Helper : NSObject
+
+@end
---
+++ Saltr/utils/Helper.m
@@ -0,0 +1,13 @@
+//
+//  Helper.m
+//  Saltr
+//
+//  Created by Gohar Hovhannisyan on 3/10/14.
+//  Copyright (c) 2014 Plexonic. All rights reserved.
+//
+
+#import "Helper.h"
+
+@implementation Helper
+
+@end
