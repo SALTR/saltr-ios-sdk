269a5e92753aed45355a049bc882fa8f0a0e2b30
Tigran Hakobyan <tigran.hakobyan@plexonic.com>
2014-07-14 23:51:57 +0400

    ported SLTChunkAssetRule

---
+++ Saltr/game/matching/SLTChunkAssetRule.h
@@ -0,0 +1,25 @@
+/*
+ * @file
+ * Saltr
+ *
+ * Copyright Teoken LLC. (c) 2014. All rights reserved.
+ * Copying or usage of any piece of this source code without written notice from Teoken LLC is a major crime.
+ * Այս կոդը Թեոկեն ՍՊԸ ընկերության սեփականությունն է:
+ * Առանց գրավոր թույլտվության այս կոդի պատճենահանումը կամ օգտագործումը քրեական հանցագործություն է:
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface SLTChunkAssetRule : NSObject
+
+@property(nonatomic, strong, readonly) NSString* assetId;
+
+@property(nonatomic, strong, readonly) NSString* distributionType;
+
+@property(nonatomic, assign, readonly) NSUInteger distributionValue;
+
+@property(nonatomic, strong, readonly) NSArray* stateIds;
+
+- (id)initWithAssetId:(NSString*)theAssetId distributionType:(NSString*)theDistributionType distributionValue:(NSUInteger)theDistributionValue andStateIds:(NSArray*)theStateIds;
+
+@end
