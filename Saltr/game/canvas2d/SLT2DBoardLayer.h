2e2c179a883301d1e4d11057876f289490502dd1
Tigran Hakobyan <tigran.hakobyan@plexonic.com>
2014-08-07 23:39:00 +0400

    Added / Implemented SLT2DBoardLayer
    Added / Implemented SLT2DBoardLayer

---
+++ Saltr/game/canvas2d/SLT2DBoardLayer.h
@@ -0,0 +1,37 @@
+/*
+ * @file
+ * Saltr
+ *
+ * Copyright Teoken LLC. (c) 2014. All rights reserved.
+ * Copying or usage of any piece of this source code without written notice from Teoken LLC is a major crime.
+ * Այս կոդը Թեոկեն ՍՊԸ ընկերության սեփականությունն է:
+ * Առանց գրավոր թույլտվության այս կոդի պատճենահանումը կամ օգտագործումը քրեական հանցագործություն է:
+ */
+
+#import "SLTBoardLayer.h"
+
+@class SLT2DAssetInstance;
+
+@interface SLT2DBoardLayer : SLTBoardLayer
+
+/// the asset instances
+@property (nonatomic, strong, readonly) NSMutableArray* assetInstances;
+
+/**
+ * @brief Inits an instance of @b SLT2DBoardLayer class with the given layer id and layer index
+ *
+ * @param theLayerId - the layer id
+ * @param theLayerIndex - the layer index
+ * @param theProperties - the properties
+ * @return - The instance of @b SLT2DBoardLayer class
+ */
+-(id) initWithLayerId:(NSString*)theLayerId andLayerIndex:(NSInteger)theLayerIndex;
+
+/**
+ * @brief Add SLT2DAssetInstance class instance
+ *
+ * @param theAssetInstance - the SLT2DAssetInstance instance
+ */
+-(void) addAssetInstance:(SLT2DAssetInstance *)theAssetInstance;
+
+@end
---
+++ Saltr/game/canvas2d/SLT2DBoardLayer.m
@@ -0,0 +1,33 @@
+/*
+ * @file
+ * Saltr
+ *
+ * Copyright Teoken LLC. (c) 2014. All rights reserved.
+ * Copying or usage of any piece of this source code without written notice from Teoken LLC is a major crime.
+ * Այս կոդը Թեոկեն ՍՊԸ ընկերության սեփականությունն է:
+ * Առանց գրավոր թույլտվության այս կոդի պատճենահանումը կամ օգտագործումը քրեական հանցագործություն է:
+ */
+
+#import "SLT2DBoardLayer.h"
+#import "SLT2DAssetInstance.h"
+
+
+@implementation SLT2DBoardLayer
+
+@synthesize assetInstances = _assetInstances;
+
+-(id) initWithLayerId:(NSString*)theLayerId andLayerIndex:(NSInteger)theLayerIndex
+{
+    self = [super initWithLayerId:theLayerId andLayerIndex:theLayerIndex];
+    if (self) {
+        _assetInstances = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+-(void) addAssetInstance:(SLT2DAssetInstance *)theAssetInstance
+{
+    [_assetInstances addObject:theAssetInstance];
+}
+
+@end
