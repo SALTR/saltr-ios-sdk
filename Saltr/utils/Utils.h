03aad093b21c6bac73d3edf577aa11282eb9e23a
Tigran Hakobyan <tigran.hakobyan@plexonic.com>
2014-09-03 18:07:00 +0400

    Utils class added
    Added Utils class

---
+++ Saltr/utils/Utils.h
@@ -0,0 +1,19 @@
+/*
+ * @file
+ * Saltr
+ *
+ * Copyright Teoken LLC. (c) 2014. All rights reserved.
+ * Copying or usage of any piece of this source code without written notice from Teoken LLC is a major crime.
+ * Այս կոդը Թեոկեն ՍՊԸ ընկերության սեփականությունն է:
+ * Առանց գրավոր թույլտվության այս կոդի պատճենահանումը կամ օգտագործումը քրեական հանցագործություն է:
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
+/** Formats a String in .Net-style, with curly braces ("{0}"). Does not support any
+ *  number formatting options yet. */
++ (NSString*) formatString:(NSString*)theFormat args:(NSArray*)theArgs;
+
+@end
---
+++ Saltr/utils/Utils.m
@@ -0,0 +1,26 @@
+/*
+ * @file
+ * Saltr
+ *
+ * Copyright Teoken LLC. (c) 2014. All rights reserved.
+ * Copying or usage of any piece of this source code without written notice from Teoken LLC is a major crime.
+ * Այս կոդը Թեոկեն ՍՊԸ ընկերության սեփականությունն է:
+ * Առանց գրավոր թույլտվության այս կոդի պատճենահանումը կամ օգտագործումը քրեական հանցագործություն է:
+ */
+
+#import "Utils.h"
+
+@implementation Utils
+
++ (NSString*) formatString:(NSString*)theFormat args:(NSArray*)theArgs
+{
+    for(NSUInteger i = 0; i< [theArgs count]; ++i) {
+        NSString* pattern = [NSString stringWithFormat:@"%@%i%@", @"{", i, @"}"];
+        
+        theFormat = [theFormat stringByReplacingOccurrencesOfString:pattern withString:[theArgs objectAtIndex:i] ];
+    }
+    
+    return theFormat;
+}
+
+@end
