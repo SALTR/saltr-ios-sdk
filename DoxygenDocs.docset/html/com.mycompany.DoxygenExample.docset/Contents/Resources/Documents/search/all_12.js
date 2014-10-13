3a94563a138420e72e42bbb978779369aefaae25
Tigran Hakobyan <tigran.hakobyan@plexonic.com>
2014-10-13 14:45:12 +0400

    SaltrTestApp removed
    SaltrTestApp removed

--- DoxygenDocs.docset/html/com.mycompany.DoxygenExample.docset/Contents/Resources/Documents/search/all_12.js
+++ DoxygenDocs.docset/html/com.mycompany.DoxygenExample.docset/Contents/Resources/Documents/search/all_12.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['variables',['variables',['../interface_s_l_t_resource_u_r_l_ticket.html#adb1d938df7b643166e64e307bd5bdd5b',1,'SLTResourceURLTicket']]],
-  ['version',['version',['../interface_s_l_t_level.html#af05b7a5e509a765e9df091cf88e8f8ac',1,'SLTLevel']]],
-  ['viewcontroller',['ViewController',['../interface_view_controller.html',1,'']]]
+  ['version',['version',['../interface_s_l_t_level.html#af05b7a5e509a765e9df091cf88e8f8ac',1,'SLTLevel']]]
 ];
