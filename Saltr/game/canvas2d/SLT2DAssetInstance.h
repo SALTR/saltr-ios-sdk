fb7e58ba3136350c34266bfeaf8245bf16fe6b60
Tigran Hakobyan <tigran.hakobyan@plexonic.com>
2015-01-27 15:21:21 +0400

    Added comments for SLT2DAssetInstance
    Added comments for SLT2DAssetInstance

--- Saltr/game/canvas2d/SLT2DAssetInstance.h
+++ Saltr/game/canvas2d/SLT2DAssetInstance.h
@@ -10,6 +10,9 @@
 
 #import "SLTAssetInstance.h"
 
+/// <summary>
+/// The SLT2DAssetInstance class represents the game 2D asset instance placed on board.
+/// </summary>
 @interface SLT2DAssetInstance : SLTAssetInstance
 
 /// the x
@@ -22,15 +25,15 @@
 @property (nonatomic, strong, readonly) NSNumber* rotation;
 
 /**
- * @brief Inits an instance of @b SLT2DAssetInstance class with the given token, states,  properties and rotation
+ * @brief Inits an instance of @b SLT2DAssetInstance class with the given token, states,  properties and rotation.
  *
- * @param theToken - the token
- * @param theStates - the states
- * @param theProperties - the properties
- * @param theX - the x
- * @param theY - the y
- * @param theRotation - the rotation
- * @return - The instance of @b SLT2DAssetInstance class
+ * @param theToken - The unique identifier of the asset.
+ * @param theStates - The current instance states.
+ * @param theProperties - The current instance properties.
+ * @param theX - The current instance x coordinate.
+ * @param theY - The current instance y coordinate.
+ * @param theRotation - The current instance rotation.
+ * @return - The instance of @b SLT2DAssetInstance class.
  */
 - (id)initWithToken:(NSString *)theToken states:(NSMutableArray *)theStates properties:(NSDictionary *)theProperties x:(NSNumber *)theX y:(NSNumber *)theY andRotation:(NSNumber *)theRotation;
 
